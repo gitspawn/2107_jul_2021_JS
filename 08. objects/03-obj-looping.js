50207583b7bf270f78308c572af5ad323bcdc907
gitspawn <volosgoto@gmail.com>
2021-07-23 16:55:21 +0300

    add lesson 7

---
+++ 08. objects/03-obj-looping.js
@@ -0,0 +1,5 @@
+// for...in
+// Object.keys
+// Object.values
+// Object.entries
+// Array for...of
---
+++ 08. objects/04-rest-spred-destucturing.js
@@ -0,0 +1,8 @@
+// Rest / Spresd if (amount === 0) {
+//     console.log('Введите сумму больше нуля!')
+//   } else if (amount > balance) {
+//     console.log('Недостаточно средств на счету!')
+//   } else {
+//     console.log('Операция проведена успешно!')
+//   }
+// }
